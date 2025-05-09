88308317e06eb1067cafe8f27547e7c6a3ceb570
Bhavesh <bhavesh.w1998@gmail.com>
2025-05-09 18:58:28 +0530

    Blog POST page depoymnet failed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  css: {
-    lightningcss: true,
-  },
   images: {
     domains: ['projecthemes.vercel.app'], // only if needed
   },
